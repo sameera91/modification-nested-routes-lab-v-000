41c46e309b9bb35b1fa0bff3c00338b1b4f87bc4
sameera91 <sameera.bachoti@gmail.com>
2016-04-14 00:17:22 -0400

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,11 @@
 module SongsHelper
-
+  def artist_id_field(song)
+    if song.artist.nil?
+      #select from existing artists 
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+    else
+      #enter a new artist
+      hidden_field_tag "song[artist_id]", song.artist_id
+    end
+  end
 end
